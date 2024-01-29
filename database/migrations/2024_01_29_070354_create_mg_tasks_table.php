432c7c1bab47c2d0178494a0ee3aa0fe9667d2d8
Dhanuwardhan <danu.wardhana@innovation.co.id>
2024-01-29 14:10:16 +0700

    test

---
+++ database/migrations/2024_01_29_070354_create_mg_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mg_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('mg_project')->onDelete('cascade');
+            $table->string('task_name');
+            $table->string('task_description')->nullable();
+            $table->datetime('start_date')->default(now()->toDateTimeString());
+            $table->datetime('end_date');
+            $table->string('assigned_by')->nullable();
+            $table->string('assigned_to')->nullable();
+            $table->string('percentage_task')->nullable();
+            $table->string('total_subtask_completed')->nullable();
+            $table->enum('task_status', ['onPending','onReview', 'workingOnIt', 'Completed', ])->nullable();
+            $table->enum('task_submit_status', ['earlyFinish', 'finish', 'finish in delay','overdue' ])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mg_tasks');
+    }
+};
