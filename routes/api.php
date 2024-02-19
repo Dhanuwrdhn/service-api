e416197b19aeb47e5adcf4e01f3d7e0fe0bd42c0
GIThuberHenry <101878909+GIThuberHenry@users.noreply.github.com>
2024-02-19 21:07:53 +0700

    chore: use middleware

--- routes/api.php
+++ routes/api.php
@@ -4,40 +4,28 @@ use Illuminate\Support\Facades\Route;
 
 // API JOBS
 require __DIR__.'/api/jobs.routes.php';
-
 // API TEAMS
 require __DIR__.'/api/teams.routes.php';
-
 // API CLIENTS
 require __DIR__.'/api/client.routes.php';
-
 // API ROLES
 require __DIR__.'/api/role.routes.php';
-
 // API PROJECTS
 require __DIR__.'/api/project.routes.php';
-
 // API EMPLOYEES
 require __DIR__.'/api/employee.routes.php';
-
 // API ATTENDANCE
 require __DIR__.'/api/attendance.routes.php';
-
 // API TASKS
 require __DIR__.'/api/task.routes.php';
-
 // API SUBTASKS
 require __DIR__.'/api/subtask.routes.php';
-
 // API EMPLOYEE PROJECTS
 require __DIR__.'/api/employeeProject.routes.php';
-
 // API EMPLOYEE TASKS
 require __DIR__.'/api/employeeTask.routes.php';
-
 // API EMPLOYEE SUBTASKS
 require __DIR__.'/api/employeeSubtask.routes.php';
-
 // API Authentication
 require __DIR__.'/api/auth.routes.php';
 
