f589eaa02817b1d9f85f833856c0a68154408ec4
Dhanuwardhan <danu.wardhana@innovation.co.id>
2024-02-22 14:22:03 +0700

    add new method

--- routes/api/employeeSubtask.routes.php
+++ routes/api/employeeSubtask.routes.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('total-employeesubtasks', [\App\Http\Controllers\EmployeeSubtasksController::class, 'showEmployeeSubTasks']);
 Route::get('total-employeesubtasks/{subtask_id}', [\App\Http\Controllers\EmployeeSubtasksController::class, 'showEmployeeSubtaskByIdSubtask']);
-Route::get('total-employeesubtasks/{employeeid}', [\App\Http\Controllers\EmployeeSubtasksController::class, 'showSubtaskEmployeeByIdEmployee']);
+Route::get('total-employeesubtasksbyemployee/{employeeid}', [\App\Http\Controllers\EmployeeSubtasksController::class, 'showSubtaskEmployeeByIdEmployee']);
