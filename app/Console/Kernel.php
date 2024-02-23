d217f06e9fc911391032712ac75faabaa83eaacf
GIThuberHenry <101878909+GIThuberHenry@users.noreply.github.com>
2024-02-23 16:31:33 +0700

    change autocheckout

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,21 +13,30 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('sanctum:purge')->daily();
+        try{
+            // $schedule->command('inspire')->hourly();
+            $schedule->command('sanctum:purge')->daily();
 
-        $schedule->call(function () {
-            // Use Laravel's HTTP client to make a PUT request to your API route
-            $response = Http::put('autocheckoutfunction', [\App\Http\Controllers\AttendanceController::class, 'autoCheckOut']);
+            $schedule->call(function () {
+                // Use Laravel's HTTP client to make a PUT request to your API route
+                $response = Http::put('autocheckoutfunction', [\App\Http\Controllers\AttendanceController::class, 'autoCheckOut']);
 
+                $discordWebhookUrl = 'https://discord.com/api/webhooks/1210505645334335521/Ke4lTZFQypZrHLYYwC2Gbwm_Dv4hwC5UunltvrSzzlb8VsXKK3e8ofrWd8hLIMih2gTP';
+
+                Http::post($discordWebhookUrl, [
+                    'content' => $response . now(),
+                ]);
+                // Log the task execution (optional)
+                \Log::info('Auto Checkout API call task executed at ' . now());
+            })->dailyAt('16:33');
+
+        }catch(\Exception $e){
             $discordWebhookUrl = 'https://discord.com/api/webhooks/1210505645334335521/Ke4lTZFQypZrHLYYwC2Gbwm_Dv4hwC5UunltvrSzzlb8VsXKK3e8ofrWd8hLIMih2gTP';
 
             Http::post($discordWebhookUrl, [
-                'content' => $response . now(),
+                'content' => 'somethingwrong ' . $e->getMessage()
             ]);
-            // Log the task execution (optional)
-            \Log::info('Auto Checkout API call task executed at ' . now());
-        })->dailyAt('16:30');
+        }
 
     }
 
