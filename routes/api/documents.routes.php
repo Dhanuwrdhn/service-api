42791e8469a96245f78cba197b2bcde85a957229
Dhanuwardhan <danu.wardhana@innovation.co.id>
2024-02-27 14:17:25 +0700

    add new method download document

--- routes/api/documents.routes.php
+++ routes/api/documents.routes.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('storeDocs', [\App\Http\Controllers\DocsController::class, 'store']);
 Route::get('getDocs', [\App\Http\Controllers\DocsController::class, 'getAll']);
 Route::get('getDoc/{id}', [\App\Http\Controllers\DocsController::class, 'getById']);
-Route::get('download/{id}', [\App\Http\Controllers\DocsController::class, 'downloadDocument']);
+Route::get('/documents/{id}/download', [\App\Http\Controllers\DocsController::class, 'downloadDocument']);
 Route::put('updateDoc/{id}', [\App\Http\Controllers\DocsController::class, 'updateDocument']);
 Route::delete('deleteDoc/{id}', [\App\Http\Controllers\DocsController::class, 'deleteDocs']);
