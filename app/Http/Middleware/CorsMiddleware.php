32cea3e24f4a433014adcb1f161b4fd8183e5e3f
GIThuberHenry <101878909+GIThuberHenry@users.noreply.github.com>
2024-03-01 16:56:10 +0700

    feat: add custom cors

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->header('Access-Control-Expose-Headers', 'Content-Length, Content-Range');
+
+        return $response;
+    }
+}
